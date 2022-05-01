0b58f2394d5b3ad47ab03f5f212df363e1293154
aceron <ariceg@comunidad.unam.mx>
2022-05-01 00:38:01 -0500

    Comment some functions

--- app/services/PartnerService.js
+++ app/services/PartnerService.js
@@ -1,12 +1,25 @@
 class PartnerSerive{
     static filterByCredits(partners, certification){
+        /**
+         * Return partners that have al least the given number of credits
+         * @param {Array} partners - Array of partners
+         * @param {Number} credits - Number of credits
+         * @returns {Array} - Array of partners
+         */
         return partners.filter(partner=> partner.credits>=certification);
     }
 
     static getAll(partners){
+        /**
+         * Return all partners given
+         */
         return partners;
     }
     static getCertificationPartners(partners){
+        /** 
+         * Return partners that have certification
+         * @param {Array} partners - Array of partners
+         */
         return partners.filter(partner=> partner.haveCertification);
     }
 
