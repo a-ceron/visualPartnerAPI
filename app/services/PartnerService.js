6d103f6ef55042d371bc76e829f4e2cc122964a2
aceron <ariceg@comunidad.unam.mx>
2022-05-01 00:11:05 -0500

    Test for read all

--- app/services/PartnerService.js
+++ app/services/PartnerService.js
@@ -2,6 +2,10 @@ class PartnerSerive{
     static filterByCredits(partners, certification){
         return partners.filter(partner=> partner.credits>=certification);
     }
+
+    static getAll(partners){
+        return partners;
+    }
 }
 
 module.exports = PartnerSerive;
--- test/services/PartnerService.test.js
+++ test/services/PartnerService.test.js
@@ -1,4 +1,5 @@
 const PartnerSerive= require('../../app/services/PartnerService');
+const Reader= require('../../app/utils/Reader');
 
 describe("Test for PartnerService class", ()=>{
     test("(1) Test for filterByCredits method", ()=>{
@@ -24,6 +25,25 @@ describe("Test for PartnerService class", ()=>{
             }
         ];
         const result= PartnerSerive.filterByCredits(partners, credits);
+        expect(result).toEqual(expected);
+    });
+    test("(2) Get all students", ()=>{
+        const partners= Reader.readFile('testfile.json');
+        const expected= [
+            {
+              "id": "6264d5d89f1df827eb84bb23",
+              "name": "Warren",
+              "email": "Todd@visualpartnership.xyz",
+              "credits": 508,
+              "enrollments": [
+                "Visual Thinking Intermedio",
+                "Visual Thinking Avanzado"
+              ],
+              "previousCourses": 1,
+              "haveCertification": true
+            }
+        ];
+        const result= PartnerSerive.getAll(partners);
         expect(result).toEqual(expected);
     });
 });
